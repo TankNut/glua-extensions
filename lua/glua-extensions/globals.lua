5f5f70d887540bca018978d45066996249f9466c
TankNut <m.vlasblom@live.nl>
2024-05-09 20:40:10 +0200

    TRACE_MAX_LENGTH global

---
+++ lua/glua-extensions/globals.lua
@@ -0,0 +1,19 @@
+--[[
+	Package: Globals
+
+	Global values that are added but don't belong to any one library or extension.
+]]
+
+-- Group: Globals
+-----------------
+
+--[[
+	Shared: TRACE_MAX_LENGTH
+
+	The minimum trace length needed to reach everything inside of a max-sized map.
+
+	--- Prototype
+		TRACE_MAX_LENGTH = 56756
+	---
+]]
+TRACE_MAX_LENGTH = 56756
